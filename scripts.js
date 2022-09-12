0416a134bd7084a2cdd095220cc313653675cfe4
aaronjpanganiban <aaron.j.panganiban@accenture.com>
2022-09-12 22:58:34 +0800

    Basic Web Dev Assessment (aaron.j.panganiban)

---
+++ scripts.js
@@ -0,0 +1,86 @@
+//*********************************************JSON*******************************************//
+let http = new XMLHttpRequest();
+http.open('get', 'products.json', true);
+http.send();
+http.onload = function(){
+   if(this.readyState == 4 && this.status == 200){
+      let products = JSON.parse(this.responseText);
+      let output = "";
+      for(let item of products){
+         output += `
+            <div class="product">
+               <img src="${item.image}" alt="${item.description}">
+               <p class="title">${item.title}</p>
+               <p class="description">${item.description}</p>
+               <p class="price">
+               <span>₱</span>
+               <span>${item.price}</span>
+               </p>
+               <p class="add-to-cart-btn">Add to cart <i class="bx bx-cart-alt"></i></p>
+            </div>
+         `;
+      }
+      document.querySelector(".products").innerHTML = output;
+   }
+}
+//*********************************************JSON*******************************************//
+
+
+//*********************************************CAROUSELL*******************************************//
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+//*********************************************CAROUSELL*******************************************//
+
+
+//*********************************************MODAL*******************************************//
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
